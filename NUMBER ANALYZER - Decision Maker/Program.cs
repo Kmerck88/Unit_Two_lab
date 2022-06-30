2cb571af1044dfab823efcc9aa461847f90219d5
Kevin Merck <kevin.merck88@gmail.com>
2022-06-30 19:09:22 -0400

    Update Program.cs
    Made some small improvements

--- NUMBER ANALYZER - Decision Maker/Program.cs
+++ NUMBER ANALYZER - Decision Maker/Program.cs
@@ -6,31 +6,33 @@ string usernum = Console.ReadLine();
 
 int userInput = int.Parse(usernum);
 
+bool isEven = num % 2 == 0 < 60; 
+
 
 //If the integer entered is odd and less than 60, print the number entered and “Odd and less than 60.”  
 //for (userInput = 0; userInput <= 100; userInput+ 2)
-    if (userInput % 2 == 1 && userInput < 60)
+    if (!isEven % 2 == 1 && userInput < 60)
 {
     Console.WriteLine("Odd and less than 60 ");
 }
-else if (userInput % 2 == 0 && userInput < 24)
+else if (isEven  % 2 == 0 && userInput < 24)
 {
     Console.WriteLine("Even and less than 25");
 }
 
 
-else if (userInput >= 26 && userInput <= 60)
+else if (isEven >= 26 && userInput <= 60)
 {
     Console.WriteLine("Even and between 26 and 60 inclusive");
 }
 
 
-else if (userInput % 2 == 0 && userInput > 60)
+else if (isEven  % 2 == 0 && userInput > 60)
 {
     Console.WriteLine("Even and greater than 60.");
 }
 
-else if (userInput % 2 == 1 && userInput > 60)
+else if (!userInput % 2 == 1 && userInput > 60)
 {
     Console.WriteLine("Odd and greater than 60");
 
